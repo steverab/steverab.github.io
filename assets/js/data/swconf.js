7b902b2518e627c4b6b901d405e88608ed7d7a4c
GitHub Action <action@github.com>
2025-02-26 01:26:23 +0000

    Build 0a2fa35dda

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,28 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1740533181',resources: [
+      '/https://rabanser.dev/assets/css/jekyll-theme-chirpy.css',
+      '/https://rabanser.dev/',
+      
+        '/https://rabanser.dev/about/',
+      
+        '/https://rabanser.dev/papers/',
+      
+        '/https://rabanser.dev/teaching/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
