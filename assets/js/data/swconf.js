0725719f3e6f8fcfac7b8ec9cceb466de1a074fa
Stephan Rabanser <stephan@cs.toronto.edu>
2025-04-26 10:11:28 -0400

    Fix base url

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1740533462',resources: [
+    cacheName: 'chirpy-1745676511',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
