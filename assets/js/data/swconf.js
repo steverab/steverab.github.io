cd9d30d199e1165799e25ce78c2939ce0207fe88
Stephan Rabanser <stephan@cs.toronto.edu>
2025-05-03 11:28:35 -0400

    Update paper

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1745676511',resources: [
+    cacheName: 'chirpy-1746285707',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
